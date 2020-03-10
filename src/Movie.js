701211cee64f2a2d102921ceb171a333b010b8a1
clairecahill <clairecahill@users.noreply.github.com>
2020-03-10 16:09:41 -0700

    Search page returns results in a table

---
+++ src/Movie.js
@@ -0,0 +1,10 @@
+/** @jsx jsx */
+import React from 'react';
+import {useState, useEffect} from 'react';
+import {jsx, css} from '@emotion/core';
+
+export default function Movie(props){
+    return (
+        
+    )
+}
