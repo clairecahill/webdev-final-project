8ca3e06953e23cbf05f3443dc7faadd32add2981
Amanda <Amanda@10-248-27-161.wireless.oregonstate.edu>
2020-03-14 10:26:04 -0700

    Added trending movies and tv shows lists

--- src/TrendingMovies.js
+++ src/TrendingMovies.js
@@ -1,15 +1,97 @@
+/** @jsx jsx */
 import React from 'react';
+import {useState, useEffect} from 'react';
+import {jsx, css} from '@emotion/core';
 
-export default class TrendingMovies extends React.Component {
-    render(){
-        return(
-            <div>
-              <h1>Trending movies</h1>
-              <p> TODO: Api call and display movies.  Amanda will build</p>
+export default function TrendingMovies(props) {
 
-            </div>
+  const [movies, setMovies] = useState([]);
 
+  function handleKeyDown(e) {
+      if(e.key === 'Enter'){
+          setSearchResults(e);
+      }
+  }
 
-        );
-    }
+  async function setSearchResults(e) {
+      // reset previous results
+      setMovies([]);
+
+      e.preventDefault();
+      let responseBody = {};
+
+      // search for movies
+      try {
+          const response = await fetch(
+          `https://api.themoviedb.org/3/trending/movie/week?api_key=a9de941ba40e3e48d10c7644969d4781&language=en-US&page=1&include_adult=false`
+          );
+          responseBody = await response.json();
+          setMovies(responseBody.results);
+      } catch(e) {
+          console.log(e)
+      }
+
+  }
+
+  function makeList(known_for){
+      let list = "";
+
+      for(let i = 0; i < known_for.length - 1; i++){
+          if (!known_for[i].title) {
+              list += known_for[i].name + ", "
+          } else {
+              list += known_for[i].title + ", "
+          }
+      }
+
+      // add the last name or title without the comma
+      if(!known_for[known_for.length - 1].title) {
+          list += known_for[known_for.length - 1].name
+      } else {
+          list += known_for[known_for.length - 1].title
+      }
+      return list
+  }
+
+  const style = css`
+      .data-row {
+          &:hover {
+              cursor: pointer;
+          }
+      }
+
+      .input-label {
+          display: block;
+      }
+  `
+
+  return(
+      <div css={style}>
+        <h1>Trending Movies of the Week.</h1>
+          <label className="input-label" for="movieSearch">Click to load trending movies this week</label>
+          <button onClick={e => setSearchResults(e)}>Load Movies</button>
+
+              {movies.length > 0 ?
+                  <table>
+                      <h3>Movies</h3>
+                      <tbody>
+                          <tr>
+                              <th>Title</th>
+                              <th>Release date</th>
+                              <th>Popularity</th>
+                          </tr>
+                      {movies.map(m =>
+                          <tr key={m.id} className="data-row"
+                              onClick={() => window.location.href=`/movie/${m.id}`}>
+                              <td>{m.title}</td>
+                              <td>{m.release_date}</td>
+                              <td>{m.popularity}</td>
+                          </tr>
+                      )}
+                      </tbody>
+                  </table>
+              : null}
+
+      </div>
+  );
 }
--- src/TrendingTV.js
+++ src/TrendingTV.js
@@ -1,9 +1,95 @@
+/** @jsx jsx */
 import React from 'react';
+import {useState, useEffect} from 'react';
+import {jsx, css} from '@emotion/core';
 
-export default class TrendingTV extends React.Component {
-    render(){
-        return(
-            <div>Trending TV</div>
-        );
-    }
-}
+export default function TrendingTV(props) {
+  const [tvShows, setTvShows] = useState([]);
+
+  function handleKeyDown(e) {
+      if(e.key === 'Enter'){
+          setSearchResults(e);
+      }
+  }
+
+  async function setSearchResults(e) {
+      // reset previous results
+      setTvShows([]);
+
+      e.preventDefault();
+      let responseBody = {};
+
+      // search for TV shows
+      try {
+          const response = await fetch(
+            `https://api.themoviedb.org/3/trending/tv/week?api_key=a9de941ba40e3e48d10c7644969d4781&language=en-US&page=1&include_adult=false`
+          );
+          responseBody = await response.json();
+          setTvShows(responseBody.results);
+      } catch(e) {
+          console.log(e)
+      }
+  }
+
+  function makeList(known_for){
+      let list = "";
+
+      for(let i = 0; i < known_for.length - 1; i++){
+          if (!known_for[i].title) {
+              list += known_for[i].name + ", "
+          } else {
+              list += known_for[i].title + ", "
+          }
+      }
+
+      // add the last name or title without the comma
+      if(!known_for[known_for.length - 1].title) {
+          list += known_for[known_for.length - 1].name
+      } else {
+          list += known_for[known_for.length - 1].title
+      }
+      return list
+  }
+
+  const style = css`
+      .data-row {
+          &:hover {
+              cursor: pointer;
+          }
+      }
+
+      .input-label {
+          display: block;
+      }
+  `
+
+  return(
+      <div css={style}>
+      <h1>Trending TV Shows of the Week.</h1>
+        <label className="input-label" for="movieSearch">Click to load trending TV Shows this week</label>
+        <button onClick={e => setSearchResults(e)}>Load Shows</button>
+
+              {tvShows.length > 0 ?
+                  <table>
+                      <h3>TV Shows</h3>
+                      <tbody>
+                          <tr>
+                              <th>Title</th>
+                              <th>First air date</th>
+                              <th>Popularity</th>
+                          </tr>
+                      {tvShows.map(t =>
+                          <tr key={t.id} className="data-row"
+                              onClick={() => window.location.href=`/tv/${t.id}`}>
+                              <td>{t.name}</td>
+                              <td>{t.first_air_date}</td>
+                              <td>{t.vote_count}</td>
+                          </tr>
+                      )}
+                      </tbody>
+                  </table>
+              : null}
+
+      </div>
+  );
+  }
